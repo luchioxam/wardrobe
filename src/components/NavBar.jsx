af287614c47e68a94ef9aeaf438876afe73f44d0
luchio <luchio012000@yahoo.fr>
2025-02-26 14:12:23 +0100

    Push

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+function NavBar (){
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+            <a class="navbar-brand" href="#">WardRobe</a>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
